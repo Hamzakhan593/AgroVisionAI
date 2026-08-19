d0200e26ef886996b176722a2b0bea7cef987db5
muhammad hamza khan <mh9813951@gmail.com>
2026-08-19 10:20:02 +0500

    Add project files.

---
+++ AgroVisionAI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgroVisionAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
